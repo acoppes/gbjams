326f289a9e34ed7a178b7af78d5908ad70b58193
Ariel Coppes <ariel.coppes@gmail.com>
2019-08-17 19:11:44 -0300

    configured first unit movement

--- Assets/Scenes/CharacterWorldView.cs
+++ Assets/Scenes/CharacterWorldView.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterWorldView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Vector3Int movement = new Vector3Int(4, 4, 0);
+    
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-movement.x, 0, 0);
+        }
         
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(movement.x, 0, 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, movement.y, 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -movement.y, 0);
+        }
     }
 }
