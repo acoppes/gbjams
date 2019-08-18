540bf6cc8ace24ac05dc478005cc824ba4687c43
Ariel Coppes <ariel.coppes@gmail.com>
2019-08-18 11:48:49 -0300

    added gb unity package from itchio to test palettes autimatically with a shader

---
+++ Assets/RogueNoodle/GBCamera/Example Elements/Scripts/FadeMaterials.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeMaterials : MonoBehaviour {
+
+	public Material[] _materials;
+	public float _fadeAmountPerSecond;
+	
+	
+	public IEnumerator FadeIn ()
+	{
+		bool complete = false;
+		
+		while (!complete)
+		{
+			complete = true;
+			
+			for (int i = 0; i < _materials.Length; i++)
+			{
+				float fade = _materials[i].GetFloat ("_Fade");
+				
+				fade = Mathf.Min (1f, fade + (_fadeAmountPerSecond * Time.deltaTime));
+				if (fade < 1f)
+				{
+					complete = false;
+				}
+				
+				_materials[i].SetFloat ("_Fade", fade);
+			}
+			
+			yield return null;
+		}
+		
+		yield break;
+		
+	}
+	
+	public IEnumerator FadeOut ()
+	{
+		bool complete = false;
+		
+		while (!complete)
+		{
+			complete = true;
+			
+			for (int i = 0; i < _materials.Length; i++)
+			{
+				float fade = _materials[i].GetFloat ("_Fade");
+				
+				fade = Mathf.Max (0f, fade - (_fadeAmountPerSecond * Time.deltaTime));
+				if (fade > 0)
+				{
+					complete = false;
+				}
+				
+				_materials[i].SetFloat ("_Fade", fade);
+			}
+			
+			yield return null;
+		}
+		
+		yield break;
+		
+	}
+	
+}
---
+++ Assets/RogueNoodle/GBCamera/Example Elements/Scripts/RandomPaletteButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RogueNoodle
+{
+namespace GBCamera
+{
+
+public class RandomPaletteButton : MonoBehaviour
+{
+	
+	public FadeMaterials _fadeMaterials;
+	public RandomPalette _randomPalette;
+	private bool _switchingPalette = false;
+	
+	public void OnChooseRandomPalette ()
+	{
+		if (!_switchingPalette)
+		{
+			StartCoroutine (ChooseRandomPalette ());
+		}
+			
+	}
+	
+	public IEnumerator ChooseRandomPalette ()
+	{
+		_switchingPalette = true;
+		yield return _fadeMaterials.StartCoroutine (_fadeMaterials.FadeOut ());
+		_randomPalette.SelectRandomPalette ();
+		yield return _fadeMaterials.StartCoroutine (_fadeMaterials.FadeIn());
+		_switchingPalette = false;
+	}
+	
+
+}
+
+
+}
+}
