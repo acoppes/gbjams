3109706cd69f4485abff897a763d624f2441b889
Ariel Coppes <ariel.coppes@gmail.com>
2019-08-25 09:54:48 -0300

    only run round position while editing

--- Assets/GBJAM7/Scripts/RoundPositionEditor.cs
+++ Assets/GBJAM7/Scripts/RoundPositionEditor.cs
@@ -7,6 +7,8 @@ namespace GBJAM7.Scripts
     {
         private void Update()
         {
+            if (Application.isPlaying)
+                return;
             var p = transform.position;
             transform.position = new Vector3(Mathf.RoundToInt(p.x), Mathf.RoundToInt(p.y), Mathf.RoundToInt(p.z));
         }
