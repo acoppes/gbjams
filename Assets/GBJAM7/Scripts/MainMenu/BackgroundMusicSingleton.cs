35bed47a7dde1421ea7cd8c179d99a69f13a4802
Ariel Coppes <ariel.coppes@gmail.com>
2019-09-04 07:46:27 -0300

    fixed music to not restart over itself

--- Assets/GBJAM7/Scripts/MainMenu/BackgroundMusicSingleton.cs
+++ Assets/GBJAM7/Scripts/MainMenu/BackgroundMusicSingleton.cs
@@ -4,9 +4,20 @@ namespace GBJAM7.Scripts.MainMenu
 {
     public class BackgroundMusicSingleton : MonoBehaviour
     {
+        private static BackgroundMusicSingleton instance;
+
+        public AudioSource bgMusic;
+        
         private void Awake()
         {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            instance = this;
             DontDestroyOnLoad(gameObject);
+            bgMusic.Play();
         }
     }
 }
