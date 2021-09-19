019151955a1b7f034d38d4ad3000e797bd7f2a66
Ariel Coppes <ariel.coppes@gmail.com>
2021-09-19 17:15:46 -0300

    testing how to use menus again xD

---
+++ gbjam9/Assets/Scenes/TestMenus/TestMenuSceneController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using GBJAM.Commons.Menus;
+using GBJAM7.Scripts.MainMenu;
+using UnityEngine;
+
+public class TestMenuSceneController : MonoBehaviour
+{
+    public OptionsMenu optionsMenu;
+
+    public string title;
+    public string[] options;
+
+    public GameboyButtonKeyMapAsset keyMapAndState;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionsMenu.title = title;
+        var optionsList = options.Select(o => new Option
+        {
+            name = o
+        }).ToList();
+        
+        optionsMenu.Show(optionsList, (i, option) =>
+        {
+            Debug.Log(option);
+            return true;
+        }, () =>
+        {
+            
+        });
+    }
+
+    private void Update()
+    {
+        keyMapAndState.UpdateControlState();
+    }
+}
