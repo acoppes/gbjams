7eba8def021cd131bb90b83bf35f94ffe40ba905
Ariel Coppes <ariel.coppes@gmail.com>
2021-09-19 16:19:13 -0300

    check for null to avoid failing in editor

--- common/Palettes/PaletteSwap.cs
+++ common/Palettes/PaletteSwap.cs
@@ -13,19 +13,24 @@ namespace GBJAM.Commons
 
         private void OnEnable()
         {
-            if (_mat == null)
+            if (_mat == null && shader != null)
+            {
                 _mat = new Material(shader);
+            }
         }
 
         private void OnDisable()
         {
             if (_mat != null)
+            {
                 DestroyImmediate(_mat);
+                _mat = null;
+            }
         }
 
         private void OnRenderImage(RenderTexture src, RenderTexture dst)
         {
-            if (paletteSelection == null)
+            if (paletteSelection == null || _mat == null || shader == null)
                 return;
             _mat.SetTexture("_PaletteTex", paletteSelection.GetCurrentPalette());
             Graphics.Blit(src, dst,  _mat);
