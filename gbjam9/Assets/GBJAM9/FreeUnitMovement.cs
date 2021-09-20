c5ffe16d1a6e8a60f5819470b79576f46494c215
Ariel Coppes <ariel.coppes@gmail.com>
2021-09-19 22:02:21 -0300

    unit flip + walking animation if moving

--- gbjam9/Assets/GBJAM9/FreeUnitMovement.cs
+++ gbjam9/Assets/GBJAM9/FreeUnitMovement.cs
@@ -16,6 +16,14 @@ namespace GBJAM9
 
         [SerializeField]
         protected Vector2 perspective = new Vector2(1.0f, 0.75f);
+
+        [SerializeField]
+        protected Animator animator;
+
+        [SerializeField]
+        protected SpriteRenderer model;
+
+        private int walkingStateHash = Animator.StringToHash("walking");
     
         // Update is called once per frame
         private void Update()
@@ -29,6 +37,16 @@ namespace GBJAM9
             myPosition.y += velocity.y * perspective.y;
 
             transform.localPosition = myPosition;
+
+            if (animator != null)
+            {
+                animator.SetBool(walkingStateHash, velocity.SqrMagnitude() > 0);
+            }
+
+            if (model != null && Mathf.Abs(velocity.x) > 0)
+            {
+                model.flipX = velocity.x < 0;
+            }
         }
     }
 }
--- gbjam9/devstuff/TODO.md
+++ gbjam9/devstuff/TODO.md
@@ -3,5 +3,4 @@ TODO
 * Room layout + editor or something
   - Easy to make rooms by other devs
   - Auto load possible rooms from folder (asset)
-* Unit movement with dpad
-* Camera follow unit
+* Unit animation while walking
