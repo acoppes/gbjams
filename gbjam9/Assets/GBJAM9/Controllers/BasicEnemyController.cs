29b0fe6336db20bd9d58f57e4c96353e226b0ea8
Ariel Coppes <ariel.coppes@gmail.com>
2021-09-23 13:21:30 -0300

    basic wander flip direction for enemy

--- gbjam9/Assets/GBJAM9/Controllers/BasicEnemyController.cs
+++ gbjam9/Assets/GBJAM9/Controllers/BasicEnemyController.cs
@@ -9,7 +9,6 @@ namespace GBJAM9.Controllers
     {
         public enum State
         {
-            Idle,
             Wander,
             FollowingPlayer,
             ReturningToWander
@@ -21,10 +20,17 @@ namespace GBJAM9.Controllers
 
         private Vector2 wanderCenter;
 
+        public Vector2 startingWanderDirection;
+
+        private float wanderSwitchDirectionCooldown;
+        
+        // TODO: use spawn parameters for this, like the spawn point or something
+        // or use variants
+
         public void Start()
         {
             entity = GetComponent<Entity>();
-            state = State.Idle;
+            state = State.Wander;
             wanderCenter = transform.position;
         }
 
@@ -33,7 +39,18 @@ namespace GBJAM9.Controllers
             // decide stuff
             
             // entity.world.GetEntityList<>()
-            entity.input.movementDirection = Vector2.right;
+            if (state == State.Wander)
+            {
+                wanderSwitchDirectionCooldown -= Time.deltaTime;
+                
+                entity.input.movementDirection = startingWanderDirection;
+                if (entity.colliderComponent.inCollision && wanderSwitchDirectionCooldown < 0)
+                {
+                    startingWanderDirection *= -1;
+                    wanderSwitchDirectionCooldown = 0.2f;
+                }
+            }
+           
         }
     }
 }
