53445aa09608e50f103dc64d4973f19ef8250935
Ariel Coppes <ariel.coppes@gmail.com>
2021-09-26 20:05:27 -0300

    copy credits + intro idea from orbital wars

---
+++ gbjam9/Assets/GBJAM9/MainMenu/MainMenuIntro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace GBJAM7.Scripts.MainMenu
+{
+    public class MainMenuIntro : MonoBehaviour
+    {
+        public Animator animator;
+        
+        [NonSerialized]
+        public bool completed;
+
+        public GameObject pressStartObject;
+
+        public Font font;
+
+        private void Awake()
+        {
+            if (font != null)
+            {
+                var texture = font.material.mainTexture;
+                texture.filterMode = FilterMode.Point;
+            }
+        }
+
+        public void OnCompleted()
+        {
+            completed = true;
+        }
+
+        public void ForceComplete()
+        {
+            completed = true;
+            animator.Play("Idle", -1, 0);
+        }
+
+        public void HideStart()
+        {
+            pressStartObject.SetActive(false);
+        }
+
+        public void ShowStart()
+        {
+            pressStartObject.SetActive(true);
+        }
+    }
+}
