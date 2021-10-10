5e5a8fbbd0124335a7cacfb10e35186eae60089a
Ariel Coppes <ariel.coppes@gmail.com>
2021-10-09 22:48:28 -0300

    script to upload to itchio

---
+++ gbjam9/upload-itchio-all.sh
@@ -0,0 +1,4 @@
+butler push builds/windows arielsan/neko-sama:windows-latest
+butler push builds/macos arielsan/neko-sama:mac-latest
+butler push builds/linux arielsan/neko-sama:linux-latest
+butler push builds/html5 arielsan/neko-sama:html5-latest
