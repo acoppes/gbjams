5447ee13e973564826916963dfbf9e828a75130b
Ariel Coppes <ariel.coppes@gmail.com>
2021-10-10 18:39:57 -0300

    first version of changelog ui

--- gbjam9/Assets/GBJAM9/Changelogs/ChangelogUI.cs
+++ gbjam9/Assets/GBJAM9/Changelogs/ChangelogUI.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GBJAM.Commons;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,10 +18,36 @@ namespace GBJAM9.Changelogs
         public TextAsset changelogFile;
         
         // TODO: gb controls
+
+        private List<string> changelogPages = new List<string>();
+
+        private int currentPage = 0;
+
+        public GameboyButtonKeyMapAsset controls;
         
         private void Start()
         {
-            changelogText.text = changelogFile.text;
+            // We assume first page is blank
+            changelogPages = changelogFile.text.Split('#').ToList();
+            changelogPages.RemoveAt(0);
+            
+            changelogText.text = changelogPages[currentPage];
+        }
+
+        private void Update()
+        {
+            nextPageObject.SetActive(currentPage + 1 < changelogPages.Count);
+            
+            if (controls.button1Pressed && nextPageObject.activeSelf)
+            {
+                currentPage++;
+                changelogText.text = changelogPages[currentPage];
+            }
+
+            if (controls.button2Pressed)
+            {
+                // CloseWindow();
+            }
         }
     }
 }
