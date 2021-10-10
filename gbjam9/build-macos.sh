895dde1e682bfdf6feb1f9253a37fc98e4caefdd
Ariel Coppes <ariel.coppes@gmail.com>
2021-10-09 23:19:51 -0300

    changed to use specific env var for unity and this project

--- gbjam9/build-macos.sh
+++ gbjam9/build-macos.sh
@@ -1,6 +1,6 @@
 GB_GAME_NAME="Nekosama"
 
-BUILD_COMMAND="${UNITY_EDITOR_PATH} -buildOSXUniversalPlayer builds/macos/${GB_GAME_NAME}.app -buildTarget OSXUniversal -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
+BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -buildOSXUniversalPlayer builds/macos/${GB_GAME_NAME}.app -buildTarget OSXUniversal -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
