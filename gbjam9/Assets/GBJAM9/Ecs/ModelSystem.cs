a46a349070c964c480f8fc235df5ef4be1193a0f
Ariel Coppes <ariel.coppes@gmail.com>
2022-05-17 21:24:23 -0300

    use events for model creation/destruction

--- gbjam9/Assets/GBJAM9/Ecs/ModelSystem.cs
+++ gbjam9/Assets/GBJAM9/Ecs/ModelSystem.cs
@@ -3,22 +3,54 @@ using Leopotam.EcsLite;
 
 namespace GBJAM9.Ecs
 {
-    public class ModelSystem : BaseSystem, IEcsRunSystem, IFixedUpdateSystem
+    public class ModelSystem : BaseSystem, IEcsRunSystem, IFixedUpdateSystem, IEcsInitSystem
     {
+        public void Init(EcsSystems systems)
+        {
+            world.onEntityCreated += OnEntityCreated;
+            world.onEntityDestroyed += OnEntityDestroyed;
+        }
+        
+        private void OnEntityCreated(Gemserk.Leopotam.Ecs.World world, int entity)
+        {
+            // create model if model component
+            var models = world.GetComponents<UnitModelComponent>();
+            if (models.Has(entity))
+            {
+                ref var model = ref models.Get(entity);
+                model.instance =  Instantiate(model.prefab);
+            }
+        }
+
+        private void OnEntityDestroyed(Gemserk.Leopotam.Ecs.World world, int entity)
+        {
+            // destroy model if model component
+            var models = world.GetComponents<UnitModelComponent>();
+            if (models.Has(entity))
+            {
+                ref var model = ref models.Get(entity);
+                if (model.instance != null)
+                {
+                    Destroy(model.instance);
+                }
+                model.instance = null;
+            }
+        }
+
         public void Run(EcsSystems systems)
         {
             var models = world.GetComponents<UnitModelComponent>();
             var positions = world.GetComponents<PositionComponent>();
 
-            foreach (var entity in world.GetFilter<UnitModelComponent>().End())
-            {
-                ref var modelComponent = ref models.Get(entity);
-
-                if (modelComponent.prefab != null && modelComponent.instance == null)
-                {
-                    modelComponent.instance = Instantiate(modelComponent.prefab);
-                }
-            }
+            // foreach (var entity in world.GetFilter<UnitModelComponent>().End())
+            // {
+            //     ref var modelComponent = ref models.Get(entity);
+            //
+            //     if (modelComponent.prefab != null && modelComponent.instance == null)
+            //     {
+            //         modelComponent.instance = Instantiate(modelComponent.prefab);
+            //     }
+            // }
             
             foreach (var entity in world.GetFilter<UnitModelComponent>().Inc<PositionComponent>().End())
             {
@@ -28,5 +60,7 @@ namespace GBJAM9.Ecs
                 modelComponent.instance.transform.position = positionComponent.value;
             }
         }
+
+
     }
 }
