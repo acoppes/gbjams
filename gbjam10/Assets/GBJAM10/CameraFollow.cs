f41da6a85cd518572b71400ed034671fd406a00c
Ariel Coppes <ariel.coppes@gmail.com>
2022-09-17 22:33:16 -0300

    camera follow only x

--- gbjam10/Assets/GBJAM10/CameraFollow.cs
+++ gbjam10/Assets/GBJAM10/CameraFollow.cs
@@ -5,9 +5,19 @@ namespace GBJAM10
 {
     public class CameraFollow : MonoBehaviour
     {
+        public enum FollowType
+        {
+            Both = 0,
+            OnlyX = 1
+        }
+        
         [FormerlySerializedAs("transform")] 
         public Transform followTransform;
         public Transform cameraTransform;
+
+        public FollowType followType;
+
+        public Vector2 offset;
 
         private void LateUpdate()
         {
@@ -15,8 +25,13 @@ namespace GBJAM10
                 return;
             
             var p = cameraTransform.transform.position;
-            p.x = followTransform.position.x;
-            p.y = followTransform.position.y;
+            p.x = followTransform.position.x + offset.x;
+            
+            if (followType != FollowType.OnlyX)
+            {
+                p.y = followTransform.position.y + offset.y;
+            }
+            
             cameraTransform.transform.position = p;
 
         }
