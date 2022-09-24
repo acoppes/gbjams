015f3d43507854a7e925731968c6c48bbc44e70e
Ariel Coppes <ariel.coppes@gmail.com>
2022-09-24 15:31:42 -0300

    fixes for scripts

--- gbjam10/build-linux.sh
+++ gbjam10/build-linux.sh
@@ -1,6 +1,6 @@
 GB_GAME_NAME="GBJAM10"
 
-BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -buildLinux64Player builds/linux/${GB_GAME_NAME} -buildTarget Linux64 -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
+BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -projectPath . -buildLinux64Player builds/linux/${GB_GAME_NAME} -buildTarget Linux64 -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
--- gbjam10/build-macos.sh
+++ gbjam10/build-macos.sh
@@ -1,6 +1,6 @@
 GB_GAME_NAME="GBJAM10"
 
-BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -buildOSXUniversalPlayer builds/macos/${GB_GAME_NAME}.app -buildTarget OSXUniversal -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
+BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -projectPath . -buildOSXUniversalPlayer builds/macos/${GB_GAME_NAME}.app -buildTarget OSXUniversal -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
