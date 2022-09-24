0e7881aa35b7fc9c33e9cd76cbeb5f21ec30f6aa
Ariel Coppes <ariel.coppes@gmail.com>
2022-09-24 13:36:53 -0300

    try to generate level beforehand but to slow

--- gbjam10/Assets/Scenes/MigrationEcs/GameController.cs
+++ gbjam10/Assets/Scenes/MigrationEcs/GameController.cs
@@ -7,10 +7,13 @@ using UnityEngine;
 public class GameController : ControllerBase
 {
     public string followName = "Main_Camera";
-
+    
     public Transform level;
     
     private bool initialized;
+
+    public int maxChunks = 500;
+    public LevelDataAsset levelData;
 
     public override void OnUpdate(float dt)
     {
@@ -28,6 +31,18 @@ public class GameController : ControllerBase
                 var model = world.GetComponent<UnitModelComponent>(followEntity);
                 cameraFollow.followTransform = model.instance.transform;
             }
+
+            var startPosition = new Vector3(0, 0, 0);
+            for (int i = 0; i < maxChunks; i++)
+            {
+                var chunkPrefab = levelData.chunksList[UnityEngine.Random.Range(0, levelData.chunksList.Count)];
+                var chunkInstance = Instantiate(chunkPrefab, level);
+                chunkInstance.transform.position = startPosition;
+                
+                startPosition += new Vector3(50, 0, 0);
+            }
+
+            initialized = true;
         }
         
     }
