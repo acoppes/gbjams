8b1f4dc6a43ab03aa7d54cd43e1531c4dd0f6b8a
Ariel Coppes <ariel.coppes@gmail.com>
2022-09-24 14:38:03 -0300

    fixed namespace

--- gbjam10/build-html5.sh
+++ gbjam10/build-html5.sh
@@ -4,7 +4,7 @@ export GB_GAME_BUILD_LOG_PATH="builds/html5.txt"
 
 export WSLENV=$WSLENV:GB_GAME_BUILD_PATH/w
 
-BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -forgetProjectPath -quit -silent-crashes -batchmode -nographics -logFile ${GB_GAME_BUILD_LOG_PATH} -executeMethod GBJAM9.Editor.BuildScript.BuildWebGL"
+BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -forgetProjectPath -quit -silent-crashes -batchmode -nographics -logFile ${GB_GAME_BUILD_LOG_PATH} -executeMethod GBJAM10.Editor.BuildScript.BuildWebGL"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
