752fb00979bbe1b4fb8431922c181235a7f8cd7c
Ariel Coppes <ariel.coppes@gmail.com>
2022-09-26 00:39:17 -0300

    log files

--- gbjam10/build-windows.sh
+++ gbjam10/build-windows.sh
@@ -1,6 +1,7 @@
 GB_GAME_NAME="SeedcityChasers"
+export GB_GAME_BUILD_LOG_PATH="builds/windows.txt"
 
-BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -projectPath . -buildWindows64Player builds/windows/${GB_GAME_NAME}.exe -buildTarget Win64 -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
+BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -projectPath . -buildWindows64Player builds/windows/${GB_GAME_NAME}.exe -logFile ${GB_GAME_BUILD_LOG_PATH} -buildTarget Win64 -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
