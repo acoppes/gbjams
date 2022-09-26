1b8e0eb35d7d06510780b262bef15901202403bc
Ariel Coppes <ariel.coppes@gmail.com>
2022-09-26 00:24:32 -0300

    test build all platforms

--- gbjam10/upload-itchio-all.sh
+++ gbjam10/upload-itchio-all.sh
@@ -1,4 +1,4 @@
-#butler push builds/windows arielsan/seedcity-chasers:windows-latest
-#butler push builds/macos arielsan/seedcity-chasers:mac-latest
-#butler push builds/linux arielsan/seedcity-chasers:linux-latest
+butler push builds/windows arielsan/seedcity-chasers:windows-latest
+butler push builds/macos arielsan/seedcity-chasers:mac-latest
+butler push builds/linux arielsan/seedcity-chasers:linux-latest
 butler push builds/html5 arielsan/seedcity-chasers:html5-latest
