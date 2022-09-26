1e0dcea3dd819f3a26faec55d2149b494c5eae36
Ariel Coppes <ariel.coppes@gmail.com>
2022-09-25 23:14:07 -0300

    victory music

---
+++ gbjam10/Assets/GBJAM10/MusicSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GBJAM10
+{
+    public class MusicSequence : MonoBehaviour
+    {
+        public List<AudioSource> musicList;
+
+        private int current;
+
+        private void Start()
+        {
+            if (current < musicList.Count)
+            {
+                musicList[current].Play();
+            }
+        }
+
+        private void Update()
+        {
+            if (musicList.Count == 0)
+                return;
+
+            if (!musicList[current].isPlaying)
+            {
+                current++;
+                if (current >= musicList.Count)
+                {
+                    current = 0;
+                }
+                
+                musicList[current].Play();
+            }
+            
+        }
+    }
+}
