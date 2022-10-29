5dbab7052c51a8f9246b66686d3b9a8a71f66377
Ariel Coppes <ariel.coppes@gmail.com>
2022-10-29 19:20:13 -0300

    updated paths

--- beateumup/build-linux.sh
+++ beateumup/build-linux.sh
@@ -1,7 +1,9 @@
 GB_GAME_NAME="Beatemup"
 export GB_GAME_BUILD_LOG_PATH="builds/linux.txt"
 
-BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -projectPath . -buildLinux64Player builds/linux/${GB_GAME_NAME} -logFile ${GB_GAME_BUILD_LOG_PATH} -buildTarget Linux64 -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
+export WSLENV=$WSLENV:GB_GAME_BUILD_PATH/w
+
+BUILD_COMMAND="\"${GB_UNITY_EDITOR_PATH}\" -projectPath . -buildLinux64Player builds/linux/${GB_GAME_NAME} -logFile ${GB_GAME_BUILD_LOG_PATH} -buildTarget Linux64 -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
--- beateumup/build-macos.sh
+++ beateumup/build-macos.sh
@@ -1,7 +1,9 @@
 GB_GAME_NAME="Beatemup"
 export GB_GAME_BUILD_LOG_PATH="builds/macos.txt"
 
-BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -projectPath . -buildOSXUniversalPlayer builds/macos/${GB_GAME_NAME}.app -logFile ${GB_GAME_BUILD_LOG_PATH} -buildTarget OSXUniversal -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
+export WSLENV=$WSLENV:GB_GAME_BUILD_PATH/w
+
+BUILD_COMMAND="\"${GB_UNITY_EDITOR_PATH}\" -projectPath . -buildOSXUniversalPlayer builds/macos/${GB_GAME_NAME}.app -logFile ${GB_GAME_BUILD_LOG_PATH} -buildTarget OSXUniversal -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
