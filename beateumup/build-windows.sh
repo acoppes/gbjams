bf36012e84c19195a838960385383fa1e1bd1fb4
Ariel Coppes <ariel.coppes@gmail.com>
2022-10-29 22:39:53 -0300

    changed a bit scripts

--- beateumup/build-windows.sh
+++ beateumup/build-windows.sh
@@ -1,9 +1,6 @@
 GB_GAME_NAME="Beatemup"
-export GB_GAME_BUILD_LOG_PATH="builds/windows.txt"
-
-export WSLENV=$WSLENV:GB_GAME_BUILD_PATH/w
-
+GB_GAME_BUILD_LOG_PATH="builds/windows.txt"
+WSLENV=$WSLENV:GB_GAME_BUILD_PATH/w
 BUILD_COMMAND="${GB_UNITY_EDITOR_PATH} -projectPath . -buildWindows64Player builds/windows/${GB_GAME_NAME}.exe -logFile ${GB_GAME_BUILD_LOG_PATH} -buildTarget Win64 -forgetProjectPath -quit -silent-crashes -batchmode -nographics"
-
-echo ${BUILD_COMMAND}
+echo "${BUILD_COMMAND}"
 ${BUILD_COMMAND}
