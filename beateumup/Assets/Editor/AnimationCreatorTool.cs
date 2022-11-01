62136bd4e3f5a7b4dbed459920217fc74fba94cd
Ariel Coppes <ariel.coppes@gmail.com>
2022-11-01 14:31:58 -0300

    Testing autocreate anims based on sprite names in folder

--- beateumup/Assets/Editor/AnimationCreatorTool.cs
+++ beateumup/Assets/Editor/AnimationCreatorTool.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,6 +7,17 @@ namespace Utils.Editor
 {
     public static class AnimationCreatorTool
     {
+        private class KeyFrame
+        {
+            public Sprite sprite;
+            public int frame;
+        }
+        
+        private class Animation
+        {
+            public List<KeyFrame> keyframes = new List<KeyFrame>();
+        }
+        
         [UnityEditor.MenuItem("Assets/Create Animations from Folder")]
         public static void CreateAnimationsFromFolder()
         {
@@ -18,43 +30,88 @@ namespace Utils.Editor
             }
 
             var path = AssetDatabase.GetAssetPath(activeObject);
+            var characterName = Path.GetFileNameWithoutExtension(path);
 
             var sprites = AssetDatabaseExt.FindAssets<Sprite>(new []
             {
                 path
             });
 
+            var animations = new Dictionary<string, Animation>();
+
             foreach (var sprite in sprites)
             {
-                Debug.Log(sprite.name);
+                var spriteParts = sprite.name.Split("_");
+                var animationName = spriteParts[0];
+                var frame = int.Parse(spriteParts[1]);
+                
+                // var animationName = sprite.name.Substring(0, 
+                //     sprite.name.IndexOf("_", StringComparison.OrdinalIgnoreCase));
+
+                if (!animations.ContainsKey(animationName))
+                {
+                    animations[animationName] = new Animation();
+                }
+
+                var animation = animations[animationName];
+                animation.keyframes.Add(new KeyFrame()
+                {
+                    sprite = sprite,
+                    frame = frame
+                });
+            }
+
+            foreach (var animationName in animations.Keys)
+            {
+                var animation = animations[animationName];
+                
+                var clip = new AnimationClip();
+                
+                var spriteBinding = new EditorCurveBinding
+                {
+                    type = typeof(SpriteRenderer),
+                    path = "Model",
+                    propertyName = "m_Sprite"
+                };
+
+                var fps = 60.0f;
+                var frameTime = 8;
+                
+                var frameDuration = 1.0f / fps;
+
+                var referenceKeyFrames = new ObjectReferenceKeyframe[animation.keyframes.Count + 1];
+                
+                for (var i = 0; i < animation.keyframes.Count; i++)
+                {
+                    referenceKeyFrames[i] = new ObjectReferenceKeyframe
+                    {
+                        time = animation.keyframes[i].frame * frameTime / fps,
+                        value = animation.keyframes[i].sprite
+                    };
+                }
+
+                // repeat last frame
+                if (frameTime > 1)
+                {
+                    var i = animation.keyframes.Count - 1;
+                    referenceKeyFrames[i + 1] = new ObjectReferenceKeyframe
+                    {
+                        time = ((animation.keyframes[i].frame + 1) * frameTime / fps) - frameDuration,
+                        value = animation.keyframes[i].sprite
+                    };
+                }
+                
+                AnimationUtility.SetObjectReferenceCurve(clip, spriteBinding, referenceKeyFrames);
+
+                if (!AssetDatabase.IsValidFolder($"Assets/Animations/{characterName}"))
+                {
+                    AssetDatabase.CreateFolder("Assets/Animations", $"{characterName}");
+                }
+                
+                AssetDatabase.CreateAsset(clip, $"Assets/Animations/{characterName}/{animationName}.anim");
             }
             
-            //
-            //
-            //
-            // var clip = new AnimationClip();
-            //
-            // var spriteBinding = new EditorCurveBinding();
-            // spriteBinding.type = typeof(SpriteRenderer);
-            // spriteBinding.path = "Model";
-            // spriteBinding.propertyName = "m_Sprite";
-            //
-            // var sprites = Selection.objects.OfType<Sprite>().ToList();
-            //
-            // var referenceKeyFrames = new ObjectReferenceKeyframe[sprites.Count];
-            //
-            // for (int i = 0; i < sprites.Count; i++)
-            // {
-            //     referenceKeyFrames[i] = new ObjectReferenceKeyframe
-            //     {
-            //         time = i,
-            //         value = sprites[i]
-            //     };
-            // }
-            //
-            // AnimationUtility.SetObjectReferenceCurve(clip, spriteBinding, referenceKeyFrames);
-            //
-            // AssetDatabase.CreateAsset(clip, "Assets/Test.anim");
+
         }
     }
 }
