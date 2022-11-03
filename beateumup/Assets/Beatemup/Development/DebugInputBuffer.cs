52d4ad6e2a0b367857cb927428a5ad4eeec079f7
Ariel Coppes <ariel.coppes@gmail.com>
2022-11-03 11:27:17 -0300

    Show keys in screen

--- beateumup/Assets/Beatemup/Development/DebugInputBuffer.cs
+++ beateumup/Assets/Beatemup/Development/DebugInputBuffer.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
+using Beatemup.Ecs;
+using Gemserk.Leopotam.Ecs;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DebugInputBuffer : MonoBehaviour
 {
-    public int playerInput;
-
+    // public int playerInput;
+    public string playerName;
+    
     public int bufferSize = 10;
 
     public GameObject actionPrefab;
@@ -12,6 +17,10 @@ public class DebugInputBuffer : MonoBehaviour
     public Transform layoutTransform;
 
     public List<Sprite> actionSprites = new List<Sprite>();
+
+    private World world;
+
+    private List<Image> inputBufferList = new List<Image>();
     
     public void Start()
     {
@@ -19,12 +28,37 @@ public class DebugInputBuffer : MonoBehaviour
         {
             var actionInstance = GameObject.Instantiate(actionPrefab, layoutTransform);
             actionInstance.SetActive(false);
+            
+            inputBufferList.Add(actionInstance.GetComponent<Image>());
         }
+
+        world = World.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
         // [ A, A, A, A ]
+
+        for (var i = 0; i < inputBufferList.Count; i++)
+        {
+            inputBufferList[i].gameObject.SetActive(false);
+        }
+
+        var entity = world.GetEntityByName(playerName);
+        if (entity == Entity.NullEntity)
+        {
+            return;
+        }
+
+        var controlComponent = world.GetComponent<ControlComponent>(entity);
+
+        if (controlComponent.forward.isPressed)
+        {
+            inputBufferList[0].gameObject.SetActive(true);
+            inputBufferList[0].sprite = 
+                actionSprites.Find(s => s.name.Equals("right", StringComparison.OrdinalIgnoreCase));
+        }
+        
     }
 }
