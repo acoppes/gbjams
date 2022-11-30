a31e4148658e2ac4de99c84e86c24dff5d9b90dc
Ariel Coppes <ariel.coppes@gmail.com>
2022-11-30 20:53:28 -0300

    scripts to build all

--- beateumup/upload-itchio-all.sh
+++ beateumup/upload-itchio-all.sh
@@ -1,4 +1,4 @@
-# butler push builds/windows arielsan/beatemup:windows-latest
-# butler push builds/macos arielsan/beatemup:mac-latest
-# butler push builds/linux arielsan/beatemup:linux-latest
 butler.exe push builds/html5 arielsan/beatemup:html5-latest
+butler.exe push builds/windows arielsan/beatemup:windows-latest
+butler.exe push builds/macos arielsan/beatemup:mac-latest
+butler.exe push builds/linux arielsan/beatemup:linux-latest
