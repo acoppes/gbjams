453971de56febcc3a38e411949d3a90d59183924
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-05 16:59:49 -0300

    Cleanup stuff

--- beateumup/Assets/Beatemup/Screens/MainMenuButton.cs
+++ beateumup/Assets/Beatemup/Screens/MainMenuButton.cs
@@ -15,17 +15,5 @@ namespace Beatemup.Screens
         {
             selectionCircle.SetActive(false);
         }
-        
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
