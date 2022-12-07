43abb746c83716ca774d8bb5ad840a6c14300046
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-07 10:52:48 -0300

    Testing manual texture duplicate + palette swap for different players

--- beateumup/Assets/Development/TestTextureSwapController.cs
+++ beateumup/Assets/Development/TestTextureSwapController.cs
@@ -6,11 +6,39 @@ public class TestTextureSwapController : MonoBehaviour
 {
     public SpriteRenderer spriteRendererA;
     public SpriteRenderer spriteRendererB;
+
+    public Texture2D paletteSwap;
+
+    public int swapIndex;
+
+    private Color[] colorMap;
+    
+    private Color GetColorSwap(Color color)
+    {
+        var width = paletteSwap.width;
+
+        for (int i = 0; i < width; i++)
+        {
+            var textureColor = colorMap[i];
+            var c0 = new Vector4(textureColor.r, textureColor.g, textureColor.b, textureColor.a);
+            var c1 = new Vector4(color.r, color.g, color.b, color.a);
+            
+            if (Vector4.Distance(c0, c1) < 0.01f)
+            {
+                return colorMap[i + (swapIndex * width)];
+            }
+        }
+        
+        return color;
+    }
     
     // Start is called before the first frame update
     void Start()
     {
+        colorMap = paletteSwap.GetPixels();
+        
         var sprite = spriteRendererA.sprite;
+        
         
         var texture = sprite.texture;
         var newTexture = new Texture2D(texture.width, texture.height, texture.format, false, false)
@@ -24,12 +52,7 @@ public class TestTextureSwapController : MonoBehaviour
 
         for (var i = 0; i < colors.Length; i++)
         {
-            var color = colors[i];
-            if (color.a > 0)
-            {
-                // rgb(64, 153, 255)
-                colors[i] = new Color(0, 1, 0, 1);
-            }
+            colors[i] = GetColorSwap(colors[i]);
         }
         
         newTexture.SetPixels(colors);
