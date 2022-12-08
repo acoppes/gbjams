052fe6b9adc1744b9395dd3ef1c0af21644f21d2
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-08 14:59:16 -0300

    Roadmap screen

---
+++ beateumup/Assets/Beatemup/Screens/RoadmapSceneController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Beatemup.MainMenu;
+using Beatemup.Scenes;
+using UnityEngine;
+
+namespace Beatemup.Screens
+{
+    public class RoadmapSceneController : MonoBehaviour
+    {
+        public string nextSceneName = "MainMenu";
+
+        public NotesScreen notes;
+        
+        private void Start()
+        {
+            notes.onClose += delegate
+            {
+                StartCoroutine(LoadingSceneController.LoadNextScene(nextSceneName));
+            };
+        }
+    }
+}
