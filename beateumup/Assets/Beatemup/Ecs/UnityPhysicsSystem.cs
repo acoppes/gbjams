940936554a0702d2736388c0e6f42998c14576e2
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-10 11:36:04 -0300

    Added system to update physics

---
+++ beateumup/Assets/Beatemup/Ecs/UnityPhysicsSystem.cs
@@ -0,0 +1,19 @@
+﻿using Gemserk.Leopotam.Ecs;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Beatemup.Ecs
+{
+    public class UnityPhysicsSystem : BaseSystem, IEcsRunSystem, IEcsInitSystem
+    {
+        public void Init(EcsSystems systems)
+        {
+            Physics.autoSimulation = false;
+        }
+        
+        public void Run(EcsSystems systems)
+        {
+            Physics.Simulate(Time.deltaTime);
+        }
+    }
+}
