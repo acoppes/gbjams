dae6c0d24f500f5ebc074cca5802916db7a9e114
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-15 13:18:19 -0300

    First projectile travel version

--- beateumup/Assets/Beatemup/Controllers/ProjectileController.cs
+++ beateumup/Assets/Beatemup/Controllers/ProjectileController.cs
@@ -1,20 +1,54 @@
 ﻿using Beatemup.Ecs;
 using Gemserk.Leopotam.Gameplay.Controllers;
 using Gemserk.Leopotam.Gameplay.Events;
+using UnityEngine;
 
 namespace Beatemup.Controllers
 {
-    public class ProjectileController : ControllerBase, IInit, IUpdate
+    public class ProjectileController : ControllerBase, IInit, IUpdate, IStateChanged
     {
         public void OnInit()
         {
             // var gravity = world.GetComponent<GravityComponent>(entity);
             // var gravity = GetEntityComponent<GravityComponent>();
+            
+            ref var states = ref GetComponent<StatesComponent>();
+            states.EnterState("Travel");
+        }
+        
+        public void OnEnterState()
+        {
+            ref var states = ref GetComponent<StatesComponent>();
+            ref var movement = ref GetComponent<HorizontalMovementComponent>();
+            
+            if (states.statesEntered.Contains("Travel"))
+            {
+                ref var gravity = ref GetComponent<GravityComponent>();
+                gravity.disabled = true;
+
+                movement.speed = movement.baseSpeed;
+            }
+        }
+
+        public void OnExitState()
+        {
+            
         }
         
         public void OnUpdate(float dt)
         {
+            ref var states = ref GetComponent<StatesComponent>();
+            ref var movement = ref GetComponent<HorizontalMovementComponent>();
+            ref var lookingDirection = ref GetComponent<LookingDirection>();
             
+            State state;
+            
+            if (states.TryGetState("Travel", out state))
+            {
+                movement.movingDirection = lookingDirection.value;
+            }
         }
+
+
     }
 }
