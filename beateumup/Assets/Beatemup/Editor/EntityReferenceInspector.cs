f111c962327cbca33f03f6b0104bda4d26a23705
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-20 17:14:22 -0300

    Show entity id in entity ref

---
+++ beateumup/Assets/Beatemup/Editor/EntityReferenceInspector.cs
@@ -0,0 +1,19 @@
+﻿using Beatemup.Ecs;
+using UnityEditor;
+
+namespace Beatemup.Editor
+{
+    [CustomEditor(typeof(EntityReference))]
+    [CanEditMultipleObjects]
+    public class EntityReferenceInspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var entityReference = target as EntityReference;
+            
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.LabelField("ENTITY" ,$"{entityReference.entity.entity:x8}:{entityReference.entity.generation}");
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+}
