e1965df2479fe177d9ae38051fa4073b65ca7168
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-20 10:47:56 -0300

    Working on ideas on camera shakes

---
+++ beateumup/Assets/Beatemup/Definitions/CameraShakeAsset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Beatemup.Definitions
+{
+    [Serializable]
+    public class CameraShake
+    {
+        public float duration;
+        public Vector2 magnitude;
+        public AnimationCurve decay = AnimationCurve.Linear(0, 1, 1, 0);
+    }
+    
+    [CreateAssetMenu(menuName = "Tools/Create Camera Shake", fileName = "CameraShake", order = 0)]
+    public class CameraShakeAsset : ScriptableObject
+    {
+        public CameraShake shake;
+    }
+}
--- beateumup/Assets/Development/Mockup_CameraShake/MockupCameraShakeController.cs
+++ beateumup/Assets/Development/Mockup_CameraShake/MockupCameraShakeController.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections;
+using Beatemup.Definitions;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
 namespace Development
 {
@@ -10,6 +13,11 @@ namespace Development
         
         public InputAction shake1;
         public InputAction shake2;
+
+        public CameraShake cameraShake1;
+        public CameraShake cameraShake2;
+
+        private Coroutine shakeCoroutine;
 
         private void OnEnable()
         {
@@ -23,13 +31,51 @@ namespace Development
 
             if (shake1.WasPressedThisFrame())
             {
-                cameraShakeAnimator.SetTrigger("shake1");
+                // cameraShakeAnimator.SetTrigger("shake1");
+                shakeCoroutine = StartCoroutine(Shake(cameraShake1, cameraShakeAnimator.transform));
             }
 
             if (shake2.WasPressedThisFrame())
             {
-                cameraShakeAnimator.SetTrigger("shake2");
+                // cameraShakeAnimator.SetTrigger("shake2");
+                shakeCoroutine = StartCoroutine(Shake(cameraShake2, cameraShakeAnimator.transform));
             }
+        }
+        
+        public IEnumerator Shake(CameraShake cameraShake, Transform t)
+        {
+            // var orignalPosition = Vector3.zero;
+
+            if (shakeCoroutine != null)
+            {
+                t.position = Vector3.zero;
+                StopCoroutine(shakeCoroutine);
+            }
+
+            var elapsed = 0f;
+
+            var x = cameraShake.magnitude.x * (Random.Range(-1f, 1f) > 0 ? 1f : -1f);
+            var y = cameraShake.magnitude.y * (Random.Range(-1f, 1f) > 0 ? 1f : -1f);
+            
+            while (elapsed < cameraShake.duration)
+            {
+                // float x = Random.Range(-1f, 1f) * magnitude.x;
+                // float y = Random.Range(-1f, 1f) * magnitude.y;
+
+                t.position = new Vector3(x, y, 0);
+                // elapsed += Time.deltaTime;
+                yield return new WaitForSeconds(1f/15f);
+                elapsed += 1f / 15f;
+
+                var time = elapsed / cameraShake.duration;
+                var decayValue = cameraShake.decay.Evaluate(time);
+
+                x *= -decayValue;
+                y *= -decayValue;
+            }
+            
+            t.position = Vector3.zero;
+            //t.position = orignalPosition;
         }
     }
 }
