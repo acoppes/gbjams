9923aace6c5f2f464e4ac652b895777944a6da7b
Ariel Coppes <ariel.coppes@gmail.com>
2023-09-19 11:50:02 -0300

    kunai stuck on objects

--- gbjam11/Assets/GBJAM11/Controllers/KunaiController.cs
+++ gbjam11/Assets/GBJAM11/Controllers/KunaiController.cs
@@ -1,22 +1,36 @@
-using Game.Components;
-using Game.Controllers;
 using Game.Utilities;
 using GBJAM11.Systems;
 using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
 using Gemserk.Leopotam.Ecs.Controllers;
-using Gemserk.Leopotam.Ecs.Events;
 using UnityEngine;
 
 namespace GBJAM11.Controllers
 {
     public class KunaiController : ControllerBase, IEntityCollisionEvent
     {
+        public Object stuckDefinition;
+        
         public void OnEntityCollision(World world, Entity entity, IEntityCollisionDelegate.EntityCollision entityCollision)
         {
-            Debug.Log("COLLISION!!");
+            if (entity.Get<DestroyableComponent>().destroy)
+                return;
+            
+            // Debug.Log("COLLISION!!");
             // change state, stop travelling, etc...
             
             // entity.Get<ProjectileComponent>().
+            
+            // if static obstacle, then spawn stuck kunai!!
+
+            var targetPosition = entityCollision.entity.Get<PositionComponent>().value;
+            targetPosition.y = entity.Get<PositionComponent>().value.y;
+
+            var newKunaiEntity = world.CreateEntity(stuckDefinition);
+            newKunaiEntity.Get<PositionComponent>().value = targetPosition;
+            newKunaiEntity.Get<LookingDirection>().value = entity.Get<LookingDirection>().value;
+
+            entity.Get<DestroyableComponent>().destroy = true;
         }
     }
 }
