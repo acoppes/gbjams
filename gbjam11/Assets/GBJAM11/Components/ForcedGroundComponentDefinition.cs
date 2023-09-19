4591e4b77f35f9ce73be204e14301ff5f04958dc
Ariel Coppes <ariel.coppes@gmail.com>
2023-09-19 15:24:52 -0300

    started testing new pressable stone but had to add a system to force ground position to avoid automoving by physics

---
+++ gbjam11/Assets/GBJAM11/Components/ForcedGroundComponentDefinition.cs
@@ -0,0 +1,22 @@
+﻿using Gemserk.Leopotam.Ecs;
+
+namespace GBJAM11.Components
+{
+    public struct ForcedGroundComponent : IEntityComponent
+    {
+        
+    }
+    
+    public class ForcedGroundComponentDefinition : ComponentDefinitionBase
+    {
+        public override string GetComponentName()
+        {
+            return nameof(ForcedGroundComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new ForcedGroundComponent());
+        }
+    }
+}
---
+++ gbjam11/Assets/GBJAM11/Controllers/StoneSwitchController.cs
@@ -0,0 +1,21 @@
+﻿using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Controllers;
+using Gemserk.Leopotam.Ecs.Events;
+using UnityEngine;
+
+namespace GBJAM11.Controllers
+{
+    public class StoneSwitchController : ControllerBase, IUpdate
+    {
+        public void OnUpdate(World world, Entity entity, float dt)
+        {
+            var physics = entity.Get<PhysicsComponent>();
+
+            if (physics.contactsCount > 0)
+            {
+                Debug.Log("IN CONTACT");
+            }
+        }
+    }
+}
