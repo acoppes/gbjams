8c009d82dd10ed22d50a4faca6d1dd4380b1eb2c
Ariel Coppes <ariel.coppes@gmail.com>
2023-09-19 17:35:55 -0300

    added scripts for builds

---
+++ gbjam11/upload-itchio-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROJECT_NAME=$1
+export GEMSERK_PROJECT_NAME=${PROJECT_NAME,,}
+
+butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/html5 arielsan/$GEMSERK_PROJECT_NAME:html5-latest
+butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/windows arielsan/$GEMSERK_PROJECT_NAME:windows-latest
+# butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/macos arielsan/$GEMSERK_PROJECT_NAME:mac-latest
+butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/linux arielsan/$GEMSERK_PROJECT_NAME:linux-latest
