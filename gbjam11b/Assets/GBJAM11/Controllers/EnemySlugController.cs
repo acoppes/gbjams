78bbf1ae32f7a354822f99943f6c5ec8f0db09d4
Ariel Coppes <ariel.coppes@gmail.com>
2023-09-22 18:46:55 -0300

    started enemy slug behaviours, fixed healthbar pos

---
+++ gbjam11b/Assets/GBJAM11/Controllers/EnemySlugController.cs
@@ -0,0 +1,82 @@
+﻿using Game.Components;
+using Game.Components.Abilities;
+using Game.Controllers;
+using Game.Utilities;
+using GBJAM11.Extensions;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
+using Gemserk.Leopotam.Ecs.Controllers;
+using Gemserk.Leopotam.Ecs.Events;
+
+namespace GBJAM11.Controllers
+{
+    public class EnemySlugController : ControllerBase, IUpdate, IActiveController
+    {
+        public bool CanBeInterrupted(Entity entity, IActiveController activeController)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnInterrupt(Entity entity, IActiveController activeController)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public void OnUpdate(World world, Entity entity, float dt)
+        {
+            ref var abilities = ref entity.Get<AbilitiesComponent>();
+            ref var states = ref entity.Get<StatesComponent>();
+            var chaseAbility = abilities.GetAbilityNoNullCheck("Chase");
+            ref var movement = ref entity.Get<MovementComponent>();
+            var position = entity.Get<PositionComponent>();
+            
+            if (states.TryGetState("Chase", out var chaseState))
+            {
+                // follow target
+                var abilityTarget = chaseAbility.abilityTargets[0];
+
+                if (!abilityTarget.valid)
+                {
+                    ExitChase(world, entity);
+                    return;
+                }
+                else
+                {
+                    movement.movingDirection = (abilityTarget.position - position.value).normalized;
+                    return;
+                }
+            }
+            
+            if (chaseAbility.CalculateTargets(world))
+            {
+                EnterChase(world, entity);
+                return;
+            }
+        }
+
+        private void EnterChase(World world, Entity entity)
+        {
+            ref var states = ref entity.Get<StatesComponent>();
+            ref var abilities = ref entity.Get<AbilitiesComponent>();
+            
+            var chaseAbility = abilities.GetAbilityNoNullCheck("Chase");
+            chaseAbility.targetsLocked = true;
+            chaseAbility.Start();
+            
+            states.EnterState("Chase");
+        }
+
+        private void ExitChase(World world, Entity entity)
+        {
+            ref var states = ref entity.Get<StatesComponent>();
+            ref var abilities = ref entity.Get<AbilitiesComponent>();
+            
+            var chaseAbility = abilities.GetAbilityNoNullCheck("Chase");
+            chaseAbility.targetsLocked = false;
+            chaseAbility.Stop(Ability.StopType.Completed);
+            
+            states.ExitState("Chase");
+        }
+ 
+    }
+}
---
+++ gbjam11b/Assets/GBJAM11/Extensions/AbilityExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Game.Components.Abilities;
+using Game.Utilities;
+using Gemserk.Leopotam.Ecs;
+
+namespace GBJAM11.Extensions
+{
+    public static class AbilityExtensions
+    {
+        public static bool CalculateTargets(this Ability ability, World world)
+        {
+            var targets = new List<Target>();
+            var count = world.GetTargets(ability.GetRuntimeTargetingParameters(), targets);
+
+            if (count > 0)
+            {
+                ability.CopyTargets(targets);
+            }
+            
+            return count > 0;
+        }
+    }
+}
