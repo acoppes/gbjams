e76102ed379214d3c8f73cc4ff0299d2dd9bb18a
Ariel Coppes <ariel.coppes@gmail.com>
2023-09-23 11:01:51 -0300

    started room generation, changed tileseet to be all together floor and borders to simplify

---
+++ gbjam11b/Assets/GBJAM11/LevelDesign/RoomData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GBJAM11.LevelDesign
+{
+    public class RoomData : MonoBehaviour
+    {
+        public Transform enterPosition;
+        public Transform exitPosition;
+    }
+}
