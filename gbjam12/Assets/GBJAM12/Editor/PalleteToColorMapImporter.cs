8ae96d7caafb4a2a9c64ee41ab50802d83345b1b
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-17 11:00:42 -0300

    implemented autoimporter from pal to colorset

--- gbjam12/Assets/GBJAM12/Editor/PalleteToColorMapImporter.cs
+++ gbjam12/Assets/GBJAM12/Editor/PalleteToColorMapImporter.cs
@@ -1,5 +1,6 @@
 using Game.DataAssets;
 using UnityEditor;
+using UnityEditor.AssetImporters;
 using UnityEngine;
 using File = System.IO.File;
 
@@ -18,26 +19,48 @@ public class PalleteToColorMapImporter
 
                 if (!string.IsNullOrEmpty(selectedFile))
                 {
-                    var palContents = File.ReadAllLines(selectedFile);
-                    
-                    // var type = palContents[0];
-                    // var config = palContents[1];
-                    
-                    Debug.Log($"Converting from palette {selectedFile} to selected colorSet");
-                    
-                    var colorsCount = int.Parse(palContents[2]);
-
-                    colorSet.colors = new Color[colorsCount];
-                    
-                    for (var i = 0; i < colorsCount; i++)
-                    {
-                        var colors = palContents[i + 3].Split(' ');
-                        colorSet.colors[i] = new Color(int.Parse(colors[0]) / 255f, int.Parse(colors[1]) / 255f, int.Parse(colors[2]) / 255f, 1f);
-                    }
+                    CopyPalFileContentsToColorSet(colorSet, selectedFile);
                     
                     EditorUtility.SetDirty(colorSet);
                 }
             }
+        }
+    }
+
+    public static void CopyPalFileContentsToColorSet(ColorSet colorSet, string selectedFile)
+    {
+        var palContents = File.ReadAllLines(selectedFile);
+                    
+        // var type = palContents[0];
+        // var config = palContents[1];
+                    
+        Debug.Log($"Converting from palette {selectedFile} to selected colorSet");
+                    
+        var colorsCount = int.Parse(palContents[2]);
+
+        colorSet.colors = new Color[colorsCount];
+                    
+        for (var i = 0; i < colorsCount; i++)
+        {
+            var colors = palContents[i + 3].Split(' ');
+            colorSet.colors[i] = new Color(int.Parse(colors[0]) / 255f, int.Parse(colors[1]) / 255f, int.Parse(colors[2]) / 255f, 1f);
+        }
+    }
+    
+    [ScriptedImporter(1, "pal")]
+    public class ColorSetPaletteImporter : ScriptedImporter
+    {
+        // public float m_Scale = 1;
+
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            var colorSet = ScriptableObject.CreateInstance<ColorSet>();
+            CopyPalFileContentsToColorSet(colorSet, ctx.assetPath);
+
+            // 'cube' is a GameObject and will be automatically converted into a prefab
+            // (Only the 'Main Asset' is eligible to become a Prefab.)
+            ctx.AddObjectToAsset("main obj", colorSet);
+            ctx.SetMainObject(colorSet);
         }
     }
 }
---
+++ gbjam12/Assets/GBJAM12/Utilities/TestColorSetFromObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Game.DataAssets;
+using UnityEngine;
+
+public class TestColorSetFromObject : MonoBehaviour
+{
+    public ColorSet colorSet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
