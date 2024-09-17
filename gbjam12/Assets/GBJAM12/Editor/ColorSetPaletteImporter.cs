7b20aac3a27903d86bedb40de34b9c31422c0c61
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-17 14:06:13 -0300

    integrating midis and midifile code

---
+++ gbjam12/Assets/GBJAM12/Editor/ColorSetPaletteImporter.cs
@@ -0,0 +1,20 @@
+﻿using Game.DataAssets;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+
+namespace GBJAM12.Editor
+{
+    [ScriptedImporter(1, "pal")]
+    public class ColorSetPaletteImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            var colorSet = ScriptableObject.CreateInstance<ColorSet>();
+            PalleteToColorMapImporter.CopyPalFileContentsToColorSet(colorSet, ctx.assetPath);
+            // 'cube' is a GameObject and will be automatically converted into a prefab
+            // (Only the 'Main Asset' is eligible to become a Prefab.)
+            ctx.AddObjectToAsset("main obj", colorSet);
+            ctx.SetMainObject(colorSet);
+        }
+    }
+}
--- gbjam12/Assets/GBJAM12/Editor/PalleteToColorMapImporter.cs
+++ gbjam12/Assets/GBJAM12/Editor/PalleteToColorMapImporter.cs
@@ -1,12 +1,11 @@
 using Game.DataAssets;
 using UnityEditor;
-using UnityEditor.AssetImporters;
 using UnityEngine;
 using File = System.IO.File;
 
-public class PalleteToColorMapImporter
+namespace GBJAM12.Editor
 {
-    public static class ConfigureFontTextureFilter
+    public static class PalleteToColorMapImporter
     {
         [MenuItem("GBJAM/Load Selected ColorSet From Palette")]
         public static void LoadColorSetFromPaletteFile()
@@ -20,47 +19,63 @@ public class PalleteToColorMapImporter
                 if (!string.IsNullOrEmpty(selectedFile))
                 {
                     CopyPalFileContentsToColorSet(colorSet, selectedFile);
-                    
                     EditorUtility.SetDirty(colorSet);
                 }
             }
         }
-    }
-
-    public static void CopyPalFileContentsToColorSet(ColorSet colorSet, string selectedFile)
-    {
-        var palContents = File.ReadAllLines(selectedFile);
-                    
-        // var type = palContents[0];
-        // var config = palContents[1];
-                    
-        Debug.Log($"Converting from palette {selectedFile} to selected colorSet");
-                    
-        var colorsCount = int.Parse(palContents[2]);
-
-        colorSet.colors = new Color[colorsCount];
-                    
-        for (var i = 0; i < colorsCount; i++)
+        
+        [MenuItem("GBJAM/Save Selected ColorSet To Palette")]
+        public static void SaveColorSetToPaletteFile()
         {
-            var colors = palContents[i + 3].Split(' ');
-            colorSet.colors[i] = new Color(int.Parse(colors[0]) / 255f, int.Parse(colors[1]) / 255f, int.Parse(colors[2]) / 255f, 1f);
+            var colorSet = Selection.activeObject as ColorSet;
+
+            if (colorSet)
+            { 
+                var selectedFileDestination = EditorUtility.SaveFilePanel("Select Destination", null, $"{colorSet.name}",  "pal");
+
+                if (!string.IsNullOrEmpty(selectedFileDestination))
+                {
+                    SaveColorSetToPalFile(colorSet, selectedFileDestination);
+                    EditorUtility.SetDirty(colorSet);
+                }
+            }
         }
-    }
-    
-    [ScriptedImporter(1, "pal")]
-    public class ColorSetPaletteImporter : ScriptedImporter
-    {
-        // public float m_Scale = 1;
 
-        public override void OnImportAsset(AssetImportContext ctx)
+        public static void CopyPalFileContentsToColorSet(ColorSet colorSet, string selectedFile)
         {
-            var colorSet = ScriptableObject.CreateInstance<ColorSet>();
-            CopyPalFileContentsToColorSet(colorSet, ctx.assetPath);
+            var palContents = File.ReadAllLines(selectedFile);
+                    
+            // var type = palContents[0];
+            // var config = palContents[1];
+                    
+            Debug.Log($"Converting from palette {selectedFile} to selected colorSet");
+                    
+            var colorsCount = int.Parse(palContents[2]);
 
-            // 'cube' is a GameObject and will be automatically converted into a prefab
-            // (Only the 'Main Asset' is eligible to become a Prefab.)
-            ctx.AddObjectToAsset("main obj", colorSet);
-            ctx.SetMainObject(colorSet);
+            colorSet.colors = new Color[colorsCount];
+                    
+            for (var i = 0; i < colorsCount; i++)
+            {
+                var colors = palContents[i + 3].Split(' ');
+                colorSet.colors[i] = 
+                    new Color(int.Parse(colors[0]) / 255f, int.Parse(colors[1]) / 255f, int.Parse(colors[2]) / 255f, 1f);
+            }
+        }
+        
+        public static void SaveColorSetToPalFile(ColorSet colorSet, string selectedDestinationPath)
+        {
+            var contents = new string[3 + colorSet.colors.Length];
+            contents[0] = "JASC-PAL";
+            contents[1] = "0100";
+            contents[2] = $"{colorSet.colors.Length}";
+                    
+            for (var i = 0; i < colorSet.colors.Length; i++)
+            {
+                var color = colorSet.colors[i];
+                contents[i + 3] = $"{(int)(color.r * 255f)} {(int)(color.g * 255f)} {(int)(color.b * 255f)}";
+            }
+            
+            File.WriteAllLines(selectedDestinationPath, contents);
         }
     }
 }
