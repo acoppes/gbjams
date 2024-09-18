f8fd1e004e07e6ae02852f872e77a08517d1e305
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-18 19:41:52 -0300

    testing different lanes with scroll to see speed, etc

---
+++ gbjam12/Assets/GBJAM12/DevMusicLaneOnStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GBJAM12.Utilities;
+using UnityEngine;
+
+namespace GBJAM12
+{
+    public class DevMusicLaneOnStart : MonoBehaviour
+    {
+        public MusicLane musicLane;
+        public MidiDataAsset midiDataAsset;
+        public AudioSource musicTrack;
+        public string trackName;
+        public int[] notes;
+
+        private void Start()
+        {
+            musicLane.Spawn(midiDataAsset, musicTrack, trackName, notes);
+        }
+    }
+}
---
+++ gbjam12/Assets/GBJAM12/MusicLane.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using GBJAM12.Utilities;
+using MidiParser;
+using UnityEngine;
+
+namespace GBJAM12
+{
+    public class MusicLane : MonoBehaviour
+    {
+        public Transform notesParent;
+        public GameObject notePrefab;
+        public float distancePerTick = 0.01f;
+
+        public int latencyOffsetInTicks = 0;
+
+        private AudioSource musicTrack;
+        private MidiDataAsset midiDataAsset;
+        
+        public void Spawn(MidiDataAsset midiDataAsset, AudioSource musicTrack, string trackName, int[] notes)
+        {
+            this.midiDataAsset = midiDataAsset;
+            this.musicTrack = musicTrack;
+            
+            var track = midiDataAsset.GetByName(trackName);
+            foreach (var midiEvent in track.events)
+            {
+                if (midiEvent.type == MidiEventType.NoteOn)
+                {
+                    var note = midiEvent.note;
+                    if (notes.Contains(note))
+                    {
+                        var noteInstance = GameObject.Instantiate(notePrefab, notesParent);
+                        noteInstance.transform.localPosition =
+                            new Vector3(0, (midiEvent.timeInTicks + latencyOffsetInTicks) * distancePerTick, 0);
+                        noteInstance.SetActive(true);
+                    }
+                }
+            }
+        }
+
+        public void LateUpdate()
+        {
+            var time = musicTrack.time;
+            var currentTick = Mathf.CeilToInt(midiDataAsset.ticksPerSecond * time);
+            
+            notesParent.localPosition = new Vector3(0, -currentTick * distancePerTick, 0);
+        }
+    }
+}
