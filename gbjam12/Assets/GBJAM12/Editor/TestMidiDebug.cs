49a789b3c255e5e963d757eca767e18357f1bc4a
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-18 19:03:41 -0300

    debug notes per track

--- gbjam12/Assets/GBJAM12/Editor/TestMidiDebug.cs
+++ gbjam12/Assets/GBJAM12/Editor/TestMidiDebug.cs
@@ -1,4 +1,5 @@
-﻿using MidiParser;
+﻿using System.Collections.Generic;
+using MidiParser;
 using Newtonsoft.Json;
 using UnityEditor;
 using UnityEngine;
@@ -33,6 +34,44 @@ namespace GBJAM12.Editor
                 
                 var json = JsonConvert.SerializeObject(midiFile, Formatting.Indented);
                 Debug.Log(json);
+                
+            }
+        }
+        
+        [MenuItem("GBJAM/Print Notes Per Track")]
+        public static void PrintNotesPerTrack()
+        {
+            var selectedObject = Selection.activeObject;
+            
+            if (selectedObject)
+            {
+                var midiFile = new MidiFile(AssetDatabase.GetAssetPath(selectedObject));
+
+                
+                foreach (var track in midiFile.Tracks)
+                {
+                    var notes = new HashSet<int>();
+                    var trackName = "";
+                    
+                    foreach (var textEvent in track.TextEvents)
+                    {
+                        if (textEvent.TextEventType == TextEventType.TrackName)
+                        {
+                            trackName = textEvent.Value;
+                        }
+                    }
+                    
+                    foreach (var midiEvent in track.MidiEvents)
+                    {
+                        if (midiEvent.MidiEventType == MidiEventType.NoteOn || midiEvent.MidiEventType == MidiEventType.NoteOff)
+                        {
+                            notes.Add(midiEvent.Note);
+                        }
+                    }
+                    // var json = JsonConvert.SerializeObject(midiFile, Formatting.Indented);
+                    Debug.Log($"{trackName}: [{string.Join(',', notes)}]");
+                }
+                
                 
             }
         }
