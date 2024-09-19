a282755584b8ec07017f19fe2cd6ae16154f9387
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-19 18:32:56 -0300

    uplaod script

--- gbjam12/upload-itchio-all.sh
+++ gbjam12/upload-itchio-all.sh
@@ -4,6 +4,6 @@ PROJECT_NAME=$1
 export GEMSERK_PROJECT_NAME=${PROJECT_NAME,,}
 
 butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/html5 arielsan/$GEMSERK_PROJECT_NAME:html5-latest
-butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/windows arielsan/$GEMSERK_PROJECT_NAME:windows-latest
+# butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/windows arielsan/$GEMSERK_PROJECT_NAME:windows-latest
 # butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/macos arielsan/$GEMSERK_PROJECT_NAME:mac-latest
-butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/linux arielsan/$GEMSERK_PROJECT_NAME:linux-latest
+# butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/linux arielsan/$GEMSERK_PROJECT_NAME:linux-latest
