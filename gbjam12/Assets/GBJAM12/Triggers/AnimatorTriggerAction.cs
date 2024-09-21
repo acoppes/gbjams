42b5bbb02e66bc151444c88a463eaff8c8dc30f9
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-20 22:43:59 -0300

    more work on splash

---
+++ gbjam12/Assets/GBJAM12/Triggers/AnimatorTriggerAction.cs
@@ -0,0 +1,17 @@
+﻿using Gemserk.Triggers;
+using UnityEngine;
+
+namespace GBJAM12.Triggers
+{
+    public class AnimatorTriggerAction : TriggerAction
+    {
+        public Animator targetAnimator;
+        public string triggerName;
+        
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            targetAnimator.SetTrigger(triggerName);
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
