bf06611fae04cdbadffe8cdbff7f3802432ece32
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-21 15:46:39 -0300

    working on zombie dance moves

---
+++ gbjam12/Assets/GBJAM12/Components/GamepadRumbleComponentDefinition.cs
@@ -0,0 +1,27 @@
+﻿using Gemserk.Leopotam.Ecs;
+
+namespace ShipMiner.Components
+{
+    public struct GamepadRumbleComponent : IEntityComponent
+    {
+        public float instensityMultiplier;
+        public float totalTime;
+        public float currentTime;
+        public bool active;
+    }
+    
+    public class GamepadRumbleComponentDefinition : ComponentDefinitionBase
+    {
+        public float instensityMultiplier = 1;
+        public float defaultTime = 1;
+
+        public override void Apply(World world, Entity entity)
+        {
+            entity.Add(new GamepadRumbleComponent()
+            {
+                totalTime = defaultTime,
+                instensityMultiplier = instensityMultiplier
+            });
+        }
+    }
+}
