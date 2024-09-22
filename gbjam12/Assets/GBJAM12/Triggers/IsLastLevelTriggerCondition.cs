bf69beabf57f76786519a7498f73dc6ad9fe0590
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-22 16:02:51 -0300

    multiple levels, cheat (number 0) to load next level

---
+++ gbjam12/Assets/GBJAM12/Triggers/IsLastLevelTriggerCondition.cs
@@ -0,0 +1,18 @@
+﻿using GBJAM12.Scenes;
+using Gemserk.Triggers;
+using UnityEngine;
+
+namespace GBJAM12.Triggers
+{
+    public class IsLastLevelTriggerCondition : TriggerCondition
+    {
+        public override bool Evaluate(object activator = null)
+        {
+            var current = GameController.currentLevel;
+            var gameController = GameObject.FindAnyObjectByType<GameController>();
+
+            return current >= gameController.gameConfiguration.levels.Count - 1;
+            
+        }
+    }
+}
--- gbjam12/Assets/GBJAM12/Triggers/SetCurrentLevelTriggerAction.cs
+++ gbjam12/Assets/GBJAM12/Triggers/SetCurrentLevelTriggerAction.cs
@@ -1,15 +1,33 @@
 ﻿using GBJAM12.Scenes;
 using Gemserk.Triggers;
+using MyBox;
 
 namespace GBJAM12.Triggers
 {
     public class SetCurrentLevelTriggerAction : TriggerAction
     {
+        public enum ActionType
+        {
+            Set = 0,
+            Next = 1
+        }
+
+        public ActionType actionType = ActionType.Set;
+        
+        [ConditionalField(nameof(actionType), false, ActionType.Set)]
         public int currentLevel;
         
         public override ITrigger.ExecutionResult Execute(object activator = null)
         {
-            GameController.currentLevel = currentLevel;
+            if (actionType == ActionType.Set)
+            {
+                GameController.currentLevel = currentLevel;
+            }
+            else
+            {
+                GameController.currentLevel += 1;
+            }
+            
             return ITrigger.ExecutionResult.Completed;
         }
     }
