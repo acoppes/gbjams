f3520e22f78b22f26ffe3170d072852960560ad0
Ariel Coppes <ariel.coppes@gmail.com>
2025-09-18 20:30:38 -0300

    upload script to ignore folders

--- gbjam13/upload-itchio-all.sh
+++ gbjam13/upload-itchio-all.sh
@@ -3,7 +3,7 @@
 PROJECT_NAME=gbjam13
 export GEMSERK_PROJECT_NAME=${PROJECT_NAME,,}
 
-butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/html5 arielsan/$GEMSERK_PROJECT_NAME:html5-latest
-butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/windows arielsan/$GEMSERK_PROJECT_NAME:windows-latest
-# butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/macos arielsan/$GEMSERK_PROJECT_NAME:mac-latest
-# butler.exe push --ignore "*_DoNotShip" builds/$GEMSERK_PROJECT_NAME/linux arielsan/$GEMSERK_PROJECT_NAME:linux-latest
+butler.exe push --ignore "*_DoNotShip" --ignore "*_ButDontShipItWithYourGame" builds/$GEMSERK_PROJECT_NAME/html5 arielsan/$GEMSERK_PROJECT_NAME:html5-latest
+butler.exe push --ignore "*_DoNotShip" --ignore "*_ButDontShipItWithYourGame" builds/$GEMSERK_PROJECT_NAME/windows arielsan/$GEMSERK_PROJECT_NAME:windows-latest
+# butler.exe push --ignore "*_DoNotShip" --ignore "*_ButDontShipItWithYourGame" builds/$GEMSERK_PROJECT_NAME/macos arielsan/$GEMSERK_PROJECT_NAME:mac-latest
+# butler.exe push --ignore "*_DoNotShip" --ignore "*_ButDontShipItWithYourGame" builds/$GEMSERK_PROJECT_NAME/linux arielsan/$GEMSERK_PROJECT_NAME:linux-latest
