4798f18a1777a8b76fb8bfa5997acdc564c3e260
Ariel Coppes <ariel.coppes@gmail.com>
2026-09-14 10:59:00 -0300

    changed to fix version

--- gbjam14/build-linux.sh
+++ gbjam14/build-linux.sh
@@ -8,7 +8,7 @@ export GEMSERK_GAME_BUILD_PATH="builds/${GEMSERK_PROJECT_NAME}/linux/${GEMSERK_P
 export GEMSERK_GAME_BUILD_LOG_PATH="builds/${GEMSERK_PROJECT_NAME}/linux.txt"
 # export WSLENV=$WSLENV:GEMSERK_GAME_BUILD_PATH:GEMSERK_PROJECT_NAME/w
 
-export BUILD_COMMAND="${GEMSERK_UNITY_EDITOR_PATH} -projectPath ${PROJECT_PATH} -quit -silent-crashes -batchmode -nographics -logFile ${GEMSERK_GAME_BUILD_LOG_PATH} -executeMethod Gemserk.BuildTools.Editor.BuildScript.BuildLinux"
+export BUILD_COMMAND="${GEMSERK_UNITY_EDITOR_PATH_6003} -projectPath ${PROJECT_PATH} -quit -silent-crashes -batchmode -nographics -logFile ${GEMSERK_GAME_BUILD_LOG_PATH} -executeMethod Gemserk.BuildTools.Editor.BuildScript.BuildLinux"
 
 echo "${BUILD_COMMAND}"
 ${BUILD_COMMAND}
