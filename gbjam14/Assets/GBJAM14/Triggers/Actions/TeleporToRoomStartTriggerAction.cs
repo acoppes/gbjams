763f456787dc6703db8c500ac33ea519678fb9ab
Ariel Coppes <ariel.coppes@gmail.com>
2026-09-15 09:46:18 -0300

    set room start also adjust looking direction

--- gbjam14/Assets/GBJAM14/Triggers/Actions/TeleporToRoomStartTriggerAction.cs
+++ gbjam14/Assets/GBJAM14/Triggers/Actions/TeleporToRoomStartTriggerAction.cs
@@ -3,6 +3,7 @@ using GBJAM14.Components;
 using Gemserk.Leopotam.Ecs;
 using Gemserk.Triggers;
 using Gemserk.Triggers.Queries;
+using Gemserk.Utilities;
 using UnityEngine;
 
 namespace GBJAM14.Triggers.Actions
@@ -29,6 +30,8 @@ namespace GBJAM14.Triggers.Actions
                     var room = GameObject.Find(activeRoom.roomId);
                     var start = room.transform.Find("Starts").Find(activeRoom.startId);
                     entity.Get<PositionComponent>().value = start.transform.position;
+                    entity.Get<LookingDirection>().value =
+                        new Vector2(1, 0).Rotate(start.transform.localEulerAngles.z * Mathf.Deg2Rad);
                 }
             }
             
