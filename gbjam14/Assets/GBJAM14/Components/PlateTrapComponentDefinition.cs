d9f994c4f8c2d5cd5d56f83cdba33e4e9262ee3a
Ariel Coppes <ariel.coppes@gmail.com>
2026-09-20 14:26:04 -0300

    adding new plat trap

---
+++ gbjam14/Assets/GBJAM14/Components/PlateTrapComponentDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Gemserk.Leopotam.Ecs;
+
+namespace GBJAM14.Components
+{
+    public struct PlatesTrapComponent : IEntityComponent
+    {
+        public int pressCount;
+        public bool pressed => pressCount > 0;
+    }
+    
+    public class PlateTrapComponentDefinition : ComponentDefinitionBase
+    {
+        public override void Apply(World world, Entity entity)
+        {
+            entity.Add(new PlatesTrapComponent()
+            {
+            });
+        }
+    }
+}
---
+++ gbjam14/Assets/GBJAM14/Controllers/PlateTrapController.cs
@@ -0,0 +1,58 @@
+﻿using Game.Components;
+using Game.Utilities;
+using GBJAM14.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Controllers;
+using Gemserk.Leopotam.Ecs.Events;
+using UnityEngine;
+
+namespace GBJAM14.Controllers
+{
+    public class PlateTrapController : ControllerBase, IInit, IUpdate
+    {
+        public Object pressSfxDefinition;
+        public Object unpressSfxDefinition;
+        
+        public void OnInit(World world, Entity entity)
+        {
+            var physics2DComponent = entity.Get<Physics2dComponent>();
+            physics2DComponent.collisionsEventsDelegate.onCollisionEnter += OnEntityCollision;
+            physics2DComponent.collisionsEventsDelegate.onCollisionExit += OnEntityCollisionExit;
+        }
+
+        private void OnEntityCollision(World world, Entity entity, IEntityCollisionDelegate.EntityCollision entityCollision)
+        {
+            ref var plates = ref entity.Get<PlatesTrapComponent>();
+            plates.pressCount++;
+        }
+        
+        private void OnEntityCollisionExit(World world, Entity entity, IEntityCollisionDelegate.EntityCollision entityCollision)
+        {
+            ref var plates = ref entity.Get<PlatesTrapComponent>();
+            plates.pressCount--;
+        }
+
+        public void OnUpdate(World world, Entity entity, float dt)
+        {
+            ref var platesTrap = ref entity.Get<PlatesTrapComponent>();
+        
+            ref var animations = ref entity.Get<AnimationsComponent>();
+
+            if (platesTrap.pressed)
+            {
+                if (!animations.IsPlaying("idle-pressed"))
+                {
+                    animations.Play("idle-pressed");
+                    return;
+                }
+            } else if (!platesTrap.pressed)
+            {
+                if (!animations.IsPlaying("idle"))
+                {
+                    animations.Play("idle");
+                    return;
+                }
+            }
+        }
+    }
+}
